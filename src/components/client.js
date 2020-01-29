d0427d7977900551486abd8451271536a3349de7
swietektomasz <swietek.tomasz@protonmail.com>
2020-01-29 22:58:21 +0100

    Update client

--- src/components/client.js
+++ src/components/client.js
@@ -1,6 +1,8 @@
 import ApolloClient from "apollo-boost"
+import fetch from "isomorphic-fetch"
 
 export const client = new ApolloClient({
   uri:
     "https://cors-anywhere.herokuapp.com/https://todo-mongo-graphql-server.herokuapp.com/",
+  fetch,
 })
